9c2f6ade896bf790bac3fb0e9ba03bcd4cdfa10a
Joseph Camarena <joseph.a.camarena@gmail.com>
2018-10-12 10:03:49 -0700

    Create lab1-2.java

---
+++ lab1-2.java
@@ -0,0 +1,32 @@
+/*
+Joseph Camarena
+Math 130-01
+Instructor: Mimi Rasky
+August 23, 2018
+Lab #1 Part 2
+ */
+
+public class Lab1_Part2
+{
+    public static void main (String [] args)
+    {
+        System.out.println("My name is Joseph and I am 6'0\" tall. My favorite TV show"); // \' not needed in Java, \" needed for a double quote
+        System.out.println("is: Hunter x Hunter. I hope to graduate from college ");
+        System.out.println("majoring in Computer Science, at the college Southwestern College ");
+        System.out.println("and eventually go to work as a Bioinformatician or Software Engineer. ");
+        System.out.print("Then, I plan on adopting a cat named Gon. After that, we both will live \n"); // \n is an escape sequence used for creating a new line
+        System.out.print("happily ever after!\n");
+    }    
+}
+
+/**
+OUTPUT:
+run:
+My name is Joseph and I am 6'0" tall. My favorite TV 
+show is: Hunter x Hunter. I hope to graduate from college 
+majoring in Computer Science, at the college Southwestern College 
+and eventually go to work as a Bioinformatician or Software Engineer. 
+Then, I plan on adopting a cat named Gon. After that, we both will live 
+happily ever after!
+BUILD SUCCESSFUL (total time: 0 seconds)
+*/
